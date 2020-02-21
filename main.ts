e2d641ab015461c0f25366bbcab3b454fc1d8b74
Maximilien Mellen <maxmellen0@gmail.com>
2020-02-21 23:32:34 +0100

    Dynamically resize canvas

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-console.log("Hi TypeScript!");
+let canvas = document.getElementById("c") as HTMLCanvasElement;
+
+if (!(canvas instanceof HTMLCanvasElement)) {
+  throw new Error("Could not find canvas.");
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+function resizeCanvas(): void {
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+
+  if (window.devicePixelRatio > 1) {
+    canvas.width *= window.devicePixelRatio;
+    canvas.height *= window.devicePixelRatio;
+  }
+}
