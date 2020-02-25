7d9836bc7d49eba7a704c897621b688fd5898bca
Maximilien Mellen <maxmellen0@gmail.com>
2020-02-25 18:22:16 +0100

    Reduce to single interaction

--- dist/main.js
+++ dist/main.js
@@ -1,35 +1,39 @@
+let AudioContext = window.AudioContext || window.webkitAudioContext;
 let audioData;
 let audioCtx;
 let audioSource;
 let fileInput = q(HTMLInputElement, "#file-input");
-let playButton = q(HTMLButtonElement, "#play-button");
 let fileReader = new FileReader();
+fileInput.addEventListener("click", () => {
+    audioCtx = audioCtx !== null && audioCtx !== void 0 ? audioCtx : new AudioContext();
+    if (audioSource)
+        audioSource.stop();
+    audioSource = audioCtx.createBufferSource();
+});
 fileInput.addEventListener("change", () => {
     var _a;
     let file = (_a = fileInput.files) === null || _a === void 0 ? void 0 : _a[0];
     if (!file)
         return;
+    fileInput.disabled = true;
     fileReader.readAsArrayBuffer(file);
 });
 fileReader.addEventListener("load", e => {
     audioData = fileReader.result;
-    playButton.disabled = false;
+    audioCtx.decodeAudioData(audioData, decodeSuccessCallback, decodeErrorCallback);
 });
 fileReader.addEventListener("error", () => {
     console.error("Could not read file:", fileReader.error);
-});
-playButton.addEventListener("click", () => {
-    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    audioSource = audioCtx.createBufferSource();
-    audioCtx.decodeAudioData(audioData, decodeSuccessCallback, decodeErrorCallback);
 });
 function decodeSuccessCallback(buffer) {
     audioSource.buffer = buffer;
     audioSource.connect(audioCtx.destination);
     audioSource.start();
+    fileInput.disabled = false;
 }
 function decodeErrorCallback(error) {
     console.error("Could not decode audio:", error);
+    fileInput.disabled = false;
 }
 function q(constructor, selector) {
     let element = document.querySelector(selector);
--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,50 @@
 declare global {
-  var webkitAudioContext: typeof AudioContext;
+  var webkitAudioContext: typeof window.AudioContext;
 }
 
+let AudioContext = window.AudioContext || window.webkitAudioContext;
 let audioData: ArrayBuffer;
 let audioCtx: AudioContext;
 let audioSource: AudioBufferSourceNode;
 let fileInput = q(HTMLInputElement, "#file-input");
-let playButton = q(HTMLButtonElement, "#play-button");
 let fileReader = new FileReader();
+
+fileInput.addEventListener("click", () => {
+  audioCtx = audioCtx ?? new AudioContext();
+  if (audioSource) audioSource.stop();
+  audioSource = audioCtx.createBufferSource();
+});
 
 fileInput.addEventListener("change", () => {
   let file = fileInput.files?.[0];
   if (!file) return;
+  fileInput.disabled = true;
   fileReader.readAsArrayBuffer(file);
 });
 
 fileReader.addEventListener("load", e => {
   audioData = fileReader.result as ArrayBuffer;
-  playButton.disabled = false;
+  audioCtx.decodeAudioData(
+    audioData,
+    decodeSuccessCallback,
+    decodeErrorCallback
+  );
 });
 
 fileReader.addEventListener("error", () => {
   console.error("Could not read file:", fileReader.error!);
 });
 
-playButton.addEventListener("click", () => {
-  audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  audioSource = audioCtx.createBufferSource();
-  audioCtx.decodeAudioData(
-    audioData!,
-    decodeSuccessCallback,
-    decodeErrorCallback
-  );
-});
-
 function decodeSuccessCallback(buffer: AudioBuffer) {
   audioSource.buffer = buffer;
   audioSource.connect(audioCtx.destination);
   audioSource.start();
+  fileInput.disabled = false;
 }
 
 function decodeErrorCallback(error: any) {
   console.error("Could not decode audio:", error);
+  fileInput.disabled = false;
 }
 
 function q<T extends HTMLElement>(
