4ab1d3c8d7521150a3f9383ff58bfd6e2a6921fe
Maximilien Mellen <maxmellen0@gmail.com>
2020-02-25 19:00:07 +0100

    Draw FFT spectrum

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,49 @@
+let fileInput = q(HTMLInputElement, "#file-input");
+let canvas = q(HTMLCanvasElement, "#c");
+let fileReader = new FileReader();
 let AudioContext = window.AudioContext || window.webkitAudioContext;
 let audioData;
 let audioCtx;
+let audioAnalyser;
 let audioSource;
-let fileInput = q(HTMLInputElement, "#file-input");
-let fileReader = new FileReader();
+let freqData;
+let positions;
+let gl = canvas.getContext("webgl");
+if (!gl)
+    throw new Error("Could not get WebGL context.");
+{
+    let vertGlsl = `
+    attribute vec2 a_position;
+    varying float v_g;
+    
+    void main() {
+      gl_Position = vec4((a_position * 2.0) - 1.0, 0.0, 1.0);
+      v_g = a_position.y;
+    }
+  `;
+    let fragGlsl = `
+    precision mediump float;
+    varying float v_g;
+    
+    void main() {
+      gl_FragColor = vec4(1.0, v_g, 0.5, 1.0);
+    }
+  `;
+    let program = compileProgram(gl, vertGlsl, fragGlsl);
+    let aPositionLocation = gl.getAttribLocation(program, "a_position");
+    let vertexBuffer = gl.createBuffer();
+    gl.useProgram(program);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.enableVertexAttribArray(aPositionLocation);
+    gl.vertexAttribPointer(aPositionLocation, 2, gl.FLOAT, false, 0, 0);
+}
 fileInput.addEventListener("click", () => {
     audioCtx = audioCtx !== null && audioCtx !== void 0 ? audioCtx : new AudioContext();
+    audioAnalyser = audioAnalyser !== null && audioAnalyser !== void 0 ? audioAnalyser : audioCtx.createAnalyser();
     if (audioSource)
         audioSource.stop();
     audioSource = audioCtx.createBufferSource();
+    audioSource.connect(audioAnalyser).connect(audioCtx.destination);
 });
 fileInput.addEventListener("change", () => {
     var _a;
@@ -27,7 +62,6 @@ fileReader.addEventListener("error", () => {
 });
 function decodeSuccessCallback(buffer) {
     audioSource.buffer = buffer;
-    audioSource.connect(audioCtx.destination);
     audioSource.start();
     fileInput.disabled = false;
 }
@@ -35,10 +69,54 @@ function decodeErrorCallback(error) {
     console.error("Could not decode audio:", error);
     fileInput.disabled = false;
 }
+window.requestAnimationFrame(function draw() {
+    if (audioAnalyser) {
+        let { frequencyBinCount, minDecibels, maxDecibels } = audioAnalyser;
+        freqData = freqData !== null && freqData !== void 0 ? freqData : new Float32Array(frequencyBinCount);
+        positions = positions !== null && positions !== void 0 ? positions : new Float32Array(freqData.length * 4);
+        audioAnalyser.getFloatFrequencyData(freqData);
+        for (let i = 0; i < freqData.length; i++) {
+            let j = i * 4;
+            let x = i / freqData.length;
+            let normalized = (freqData[i] - minDecibels) / (maxDecibels - minDecibels);
+            positions[j] = x;
+            positions[j + 1] = normalized;
+            positions[j + 2] = x;
+            positions[j + 3] = 0;
+        }
+        gl.bufferData(gl.ARRAY_BUFFER, positions, gl.DYNAMIC_DRAW);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, positions.length / 2);
+    }
+    window.requestAnimationFrame(draw);
+});
 function q(constructor, selector) {
     let element = document.querySelector(selector);
     if (!(element instanceof constructor)) {
         throw new Error(`${constructor.name} "${selector}" not found.`);
     }
     return element;
+}
+function compileProgram(gl, vertGlsl, fragGlsl) {
+    let vertShader = compileShader(gl, gl.VERTEX_SHADER, vertGlsl);
+    let fragShader = compileShader(gl, gl.FRAGMENT_SHADER, fragGlsl);
+    let program = gl.createProgram();
+    gl.attachShader(program, vertShader);
+    gl.attachShader(program, fragShader);
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        let infoLog = gl.getProgramInfoLog(program);
+        throw new Error(`Could not compile program:\n${infoLog}`);
+    }
+    return program;
+}
+function compileShader(gl, type, source) {
+    let shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        let infoLog = gl.getShaderInfoLog(shader);
+        throw new Error(`Could not compile shader:\n${infoLog}`);
+    }
+    return shader;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,58 @@ declare global {
   var webkitAudioContext: typeof window.AudioContext;
 }
 
-let AudioContext = window.AudioContext || window.webkitAudioContext;
-let audioData: ArrayBuffer;
-let audioCtx: AudioContext;
-let audioSource: AudioBufferSourceNode;
 let fileInput = q(HTMLInputElement, "#file-input");
+let canvas = q(HTMLCanvasElement, "#c");
 let fileReader = new FileReader();
+
+let AudioContext = window.AudioContext || window.webkitAudioContext;
+let audioData: ArrayBuffer | undefined;
+let audioCtx: AudioContext | undefined;
+let audioAnalyser: AnalyserNode | undefined;
+let audioSource: AudioBufferSourceNode | undefined;
+
+let freqData: Float32Array | undefined;
+let positions: Float32Array | undefined;
+
+let gl = canvas.getContext("webgl");
+if (!gl) throw new Error("Could not get WebGL context.");
+
+{
+  let vertGlsl = `
+    attribute vec2 a_position;
+    varying float v_g;
+    
+    void main() {
+      gl_Position = vec4((a_position * 2.0) - 1.0, 0.0, 1.0);
+      v_g = a_position.y;
+    }
+  `;
+
+  let fragGlsl = `
+    precision mediump float;
+    varying float v_g;
+    
+    void main() {
+      gl_FragColor = vec4(1.0, v_g, 0.5, 1.0);
+    }
+  `;
+
+  let program = compileProgram(gl, vertGlsl, fragGlsl);
+  let aPositionLocation = gl.getAttribLocation(program, "a_position");
+  let vertexBuffer = gl.createBuffer();
+
+  gl.useProgram(program);
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.enableVertexAttribArray(aPositionLocation);
+  gl.vertexAttribPointer(aPositionLocation, 2, gl.FLOAT, false, 0, 0);
+}
 
 fileInput.addEventListener("click", () => {
   audioCtx = audioCtx ?? new AudioContext();
+  audioAnalyser = audioAnalyser ?? audioCtx.createAnalyser();
   if (audioSource) audioSource.stop();
   audioSource = audioCtx.createBufferSource();
+  audioSource!.connect(audioAnalyser!).connect(audioCtx!.destination);
 });
 
 fileInput.addEventListener("change", () => {
@@ -24,7 +65,7 @@ fileInput.addEventListener("change", () => {
 
 fileReader.addEventListener("load", e => {
   audioData = fileReader.result as ArrayBuffer;
-  audioCtx.decodeAudioData(
+  audioCtx!.decodeAudioData(
     audioData,
     decodeSuccessCallback,
     decodeErrorCallback
@@ -36,9 +77,8 @@ fileReader.addEventListener("error", () => {
 });
 
 function decodeSuccessCallback(buffer: AudioBuffer) {
-  audioSource.buffer = buffer;
-  audioSource.connect(audioCtx.destination);
-  audioSource.start();
+  audioSource!.buffer = buffer;
+  audioSource!.start();
   fileInput.disabled = false;
 }
 
@@ -46,6 +86,34 @@ function decodeErrorCallback(error: any) {
   console.error("Could not decode audio:", error);
   fileInput.disabled = false;
 }
+
+window.requestAnimationFrame(function draw() {
+  if (audioAnalyser) {
+    let { frequencyBinCount, minDecibels, maxDecibels } = audioAnalyser;
+    freqData = freqData ?? new Float32Array(frequencyBinCount);
+    positions = positions ?? new Float32Array(freqData.length * 4);
+
+    audioAnalyser.getFloatFrequencyData(freqData);
+
+    for (let i = 0; i < freqData.length; i++) {
+      let j = i * 4;
+      let x = i / freqData.length;
+      let normalized =
+        (freqData[i] - minDecibels) / (maxDecibels - minDecibels);
+
+      positions[j] = x;
+      positions[j + 1] = normalized;
+      positions[j + 2] = x;
+      positions[j + 3] = 0;
+    }
+
+    gl!.bufferData(gl!.ARRAY_BUFFER, positions, gl!.DYNAMIC_DRAW);
+    gl!.clear(gl!.COLOR_BUFFER_BIT);
+    gl!.drawArrays(gl!.TRIANGLE_STRIP, 0, positions.length / 2);
+  }
+
+  window.requestAnimationFrame(draw);
+});
 
 function q<T extends HTMLElement>(
   constructor: { new (): T },
@@ -56,6 +124,44 @@ function q<T extends HTMLElement>(
     throw new Error(`${constructor.name} "${selector}" not found.`);
   }
   return element;
+}
+
+function compileProgram(
+  gl: WebGLRenderingContext,
+  vertGlsl: string,
+  fragGlsl: string
+): WebGLProgram {
+  let vertShader = compileShader(gl, gl.VERTEX_SHADER, vertGlsl);
+  let fragShader = compileShader(gl, gl.FRAGMENT_SHADER, fragGlsl);
+  let program = gl.createProgram()!;
+
+  gl.attachShader(program, vertShader);
+  gl.attachShader(program, fragShader);
+  gl.linkProgram(program);
+
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    let infoLog = gl.getProgramInfoLog(program);
+    throw new Error(`Could not compile program:\n${infoLog}`);
+  }
+
+  return program;
+}
+
+function compileShader(
+  gl: WebGLRenderingContext,
+  type: GLenum,
+  source: string
+): WebGLShader {
+  let shader = gl.createShader(type)!;
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    let infoLog = gl.getShaderInfoLog(shader);
+    throw new Error(`Could not compile shader:\n${infoLog}`);
+  }
+
+  return shader;
 }
 
 export {};
